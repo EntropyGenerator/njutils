526c0df72c04f1f616ae95eb083d8311c91075e7
int16 <int16int8@gmail.com>
2024-10-06 14:11:17 +0800

    Now it's good enough

--- dormitory_electricity/dormitory_electricity_tracer.py
+++ dormitory_electricity/dormitory_electricity_tracer.py
@@ -23,21 +23,27 @@ def draw_elec_degree():
         elec_degree=float(format(elec_degree*elec_fee,".2f"))
         elec_diff=0
         if len(elec_history)>1:
-            elec_diff=elec_history[-2][1]-elec_history[-1][1]
+            elec_diff=float(format(elec_history[-1][1]-elec_degree,".4f"))
         elec_history.append([f"{t.tm_mon}-{t.tm_mday} {t.tm_hour}:{t.tm_min}",elec_degree,elec_diff])
         print(f"[{t.tm_mon}-{t.tm_mday} {t.tm_hour}:{t.tm_min}]Successfully fetched elec info: {elec_degree} degrees.")
     else:
         print("Fetch elec info FAILED.")
 
     plt.figure(figsize=(15,4))
-    plt.style.use("ggplot")
-    plt.plot([i[0] for i in elec_history],[i[1] for i in elec_history])
-    plt.plot([i[0] for i in elec_history],[i[2] for i in elec_history])
-    plt.title("Dormitory Electricity Tracer")
+    #plt.style.use("ggplot")
+    fig,plt_degree=plt.subplots()
     plt.xticks(ticks=list(range(0,len(elec_history),10)),rotation=90)
+    plt_degree.plot([i[0] for i in elec_history],[i[1] for i in elec_history],"g-")
+    plt_degree.set_ylabel('Degrees remaining', color='g')
+	
+    plt_diff=plt_degree.twinx()
+    plt_diff.plot([i[0] for i in elec_history],[i[2] for i in elec_history],"b-")
+    plt_diff.set_ylabel("Diff",color='b')
+
+    plt.title("Dormitory Electricity Tracer")
     for i in range(0,len(elec_history),10):
-        plt.text(elec_history[i][0],elec_history[i][1],elec_history[i][1],va="bottom",ha="left",rotation=75)
-        plt.text(elec_history[i][0],elec_history[i][2],elec_history[i][2],va="bottom",ha="left",rotation=75)
+        plt_degree.text(elec_history[i][0],elec_history[i][1],elec_history[i][1],va="bottom",ha="left",rotation=75)
+        plt_diff.text(elec_history[i][0],elec_history[i][2],elec_history[i][2],va="bottom",ha="left",rotation=75)
     plt.tight_layout()
     plt.savefig(result_pic_name)
 
